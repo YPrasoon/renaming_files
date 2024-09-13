37a1db00867fd1869b08ea2153c531501e6b166c
YPrasoon <skilledabhinav@gmail.com>
2024-09-13 23:05:22 +0530

    renaming file in bulk

---
+++ README.md
@@ -0,0 +1,3 @@
+"# scripts" 
+"# renaming_files" 
+"# renaming_files" 
---
+++ Renaming_in_bulk.py
@@ -0,0 +1,29 @@
+#Renaming in bulk
+
+import os
+import pathlib 
+import time
+
+def main():
+    i = 0
+    source_folder = input("input the source folder : ")
+    workpath = pathlib.PureWindowsPath(source_folder)
+    path = workpath.as_posix() + "/"
+    #path = path + "/"
+    for filename in os.listdir(path):
+        my_dest = "testfile_" + str(i) + ".jpg"
+        my_source = path + filename
+        my_dest = path + my_dest
+        os.rename(my_source, my_dest)
+        i += 1
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print('.')
+    time.sleep(1)
+    print('.')
+    time.sleep(1)
+    print("Renaming complete")
+
+if __name__ == '__main__':
+    main()
